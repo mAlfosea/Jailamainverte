db09b518aa989e1534a2bc4df966137860f23963
Maxime <maxime.alfosea@hotmail.fr>
2019-05-03 16:56:16 +0200

    Mise place de l'app de gestion de ses plantes

---
+++ app/src/main/java/fr/perfectblue/jailamainverte/model/Plant.kt
@@ -0,0 +1,5 @@
+package fr.perfectblue.jailamainverte.model
+
+data class Plant (private var plantName: String, private var plantFamily: String, private var nextArrosage: String, private var arrosageCycle: Int) {
+
+}
